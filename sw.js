4e3998f6a7e6197a7ae583b9591120896a3b6c24
gustavogabrielgomes12345-sketch <gustavogabrielgomes12345@gmail.com>
2026-05-30 15:45:20 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'app-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('SW instalado');
+
+  self.skipWaiting(); 
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('SW ativado');
+
+  return self.clients.claim(); 
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.skipWaiting();
+self.clients.claim();
